6d23b5154d5172c4d639d5bae24188a28aeb3842
goldmagala56-gif <goldmagala56@gmail.com>
2026-02-26 18:01:05 +0300

    Add Player and Game models to database

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
+from database import db
 
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey("player.id"))
+    secret_number = db.Column(db.Integer)
+    attempts = db.Column(db.Integer, default=0)
+    completed = db.Column(db.Boolean, default=False)
